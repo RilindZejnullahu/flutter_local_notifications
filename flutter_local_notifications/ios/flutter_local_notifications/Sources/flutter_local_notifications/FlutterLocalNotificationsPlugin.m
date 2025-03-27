962c2af1c83809d4dd90c6fd6f7511f02bb6380a
Anka <runner@Mac-1743084287948.local>
2025-03-27 14:10:06 +0000

    Clang Format

--- flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
+++ flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
@@ -822,18 +822,17 @@ static FlutterError *getFlutterError(NSError *error) {
         triggerWithTimeInterval:60 * 60 * 24 * 7 * 2
                         repeats:YES];
   case EveryFourWeeks: {
-      NSTimeInterval interval = 60 * 60 * 24 * 7 * 4;
-      
-      // Log current time and expected firing time
-      NSDate *currentDate = [NSDate date];
-      NSDate *expectedFireDate = [currentDate dateByAddingTimeInterval:interval];
-      
-      NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-      formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss Z";
-      
-      return [UNTimeIntervalNotificationTrigger
-          triggerWithTimeInterval:interval
-                          repeats:YES];
+    NSTimeInterval interval = 60 * 60 * 24 * 7 * 4;
+
+    // Log current time and expected firing time
+    NSDate *currentDate = [NSDate date];
+    NSDate *expectedFireDate = [currentDate dateByAddingTimeInterval:interval];
+
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss Z";
+
+    return [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:interval
+                                                              repeats:YES];
   }
   }
   return nil;

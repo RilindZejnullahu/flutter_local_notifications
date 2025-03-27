f22beda0128ed610f227c676b9fc8519c292146b
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2025-03-27 14:45:09 +0100

    Two tests

--- flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
+++ flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
@@ -817,12 +817,21 @@ static FlutterError *getFlutterError(NSError *error) {
                         repeats:YES];
   case BiWeekly:
     return [UNTimeIntervalNotificationTrigger
-        triggerWithTimeInterval:60 * 60 * 24 * 7 * 2
+        triggerWithTimeInterval:(60 * 60 * 24 * 7 * 2) - 3600
                         repeats:YES];
-  case EveryFourWeeks:
-    return [UNTimeIntervalNotificationTrigger
-        triggerWithTimeInterval:60 * 60 * 24 * 7 * 4
-                        repeats:YES];
+  case BiWeekly: {
+      NSTimeInterval interval = 60 * 60 * 24 * 7 * 4;
+      
+      // Log current time and expected firing time
+      NSDate *currentDate = [NSDate date];
+      NSDate *expectedFireDate = [currentDate dateByAddingTimeInterval:interval];
+      
+      NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+      formatter.dateFormat = @"yyyy-MM-dd HH:mm:ss Z";
+      
+      return [UNTimeIntervalNotificationTrigger
+          triggerWithTimeInterval:interval
+                          repeats:YES];
   }
   return nil;
 }

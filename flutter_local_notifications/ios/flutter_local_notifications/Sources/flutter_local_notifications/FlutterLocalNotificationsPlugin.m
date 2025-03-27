4291cc5ef6d499a04129ae408219a1bfbb115be4
Rilind Zejnullahu <58613659+RilindZejnullahu@users.noreply.github.com>
2025-03-27 14:12:16 +0100

    more tests

--- flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
+++ flutter_local_notifications/ios/flutter_local_notifications/Sources/flutter_local_notifications/FlutterLocalNotificationsPlugin.m
@@ -833,11 +833,21 @@ static FlutterError *getFlutterError(NSError *error) {
         triggerWithTimeInterval:(60 * 60 * 24 * 7 * 2) - 3600
                         repeats:YES];
   case EveryFourWeeks:
-    NSLog(@"Creating EveryFourWeeks trigger");
-    return [UNTimeIntervalNotificationTrigger
-        triggerWithTimeInterval:60 * 60 * 24 * 7 * 4
-                        repeats:YES];
-  }
+    {
+      NSLog(@"Creating EveryFourWeeks trigger");
+      NSTimeInterval interval = 60 * 60 * 24 * 7 * 4;
+      // Check current DST offset
+      NSTimeZone *localTimeZone = [NSTimeZone localTimeZone];
+      NSInteger dstOffset = [localTimeZone daylightSavingTimeOffset];
+      // Adjust interval if needed
+      if (dstOffset != 0) {
+        NSLog(@"Adjusting for DST offset: %ld seconds", (long)dstOffset);
+        interval -= dstOffset;
+      }
+      return [UNTimeIntervalNotificationTrigger
+          triggerWithTimeInterval:interval
+                          repeats:YES];
+    }
   return nil;
 }
 
